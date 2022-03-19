50b29a90acf497d1fedaf9654448200be7a4b98f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-19 13:55:24 +0700

    impor package dotenv sekaligus memanggil fungsi config

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+// mengimpor dotenv dan menjalankan konfigurasinya
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesService');
